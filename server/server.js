c29cc64b55118446b7d9c287891dc82217b7e810
QaysTrad <qaystrad@gamil.com>
2018-09-12 11:34:20 +0300

    remove equipment from the database

--- server/server.js
+++ server/server.js
@@ -6,42 +6,51 @@ const db = require('../database/index');
 const app = express();
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
-
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(express.static(path.join(__dirname, '../angular-client/dist/angular-client')))
 
-app.post('/addEquip' , function(req , res) {
+app.post('/addEquip', (req, res) => {
 	let name = req.body.name;
 	let serialNumber = req.body.serialNumber;
 
-	let equip =new db.equipmentSchema({
+	let equip = new db.equipmentSchema({
 		name: name,
 		serialNumber: serialNumber
 	});
-	equip.save(function (err, data) { 
-        if (err) {
-            throw err; 
-        } else {
-            console.log('saved!'); 
-        }
-    });
+	equip.save((err, data) => {
+		if (err) {
+			throw err;
+		} else {
+			console.log('saved!');
+		}
+	});
 })
 
-app.get('/getEquip', function (req,res) {
-	db.equipmentSchema.find({}, function (err, data) {
+app.post('/deleteEquip', (req, res) => {
+	id = req.body.id;
+	db.equipmentSchema.findOneAndRemove({_id:id},(err, data)=>{
 		if(err){
 			throw err;
 		}else{
+            res.sendStatus(200);
+		}
+	})
+})
+app.get('/getEquip', (req, res) => {
+	db.equipmentSchema.find({}, (err, data) => {
+		if (err) {
+			throw err;
+		} else {
 			res.send(data);
 		}
 	})
 })
 
 
-app.get('/*', function (req , res) {
-	res.sendFile(path.resolve(path.join(__dirname,'../angular-client/dist/angular-client/index.html')))
+app.get('/*', function (req, res) {
+	res.sendFile(path.resolve(path.join(__dirname, '../angular-client/dist/angular-client/index.html')))
 })
 
-var port= 3000;
+var port = 3000;
 app.listen(port, () => console.log(`listening on port ${port}!`));
