b838f73896628f5b3100595b13d2e2f84e82c45e
QaysTrad <qaystrad@gamil.com>
2018-09-15 01:06:27 +0300

    fixing the style

--- database/index.js
+++ database/index.js
@@ -1,47 +1,44 @@
-let mongoose = require('mongoose') // Import mongoose module
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/manage');
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
 
-mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost/mange', { useNewUrlParser: true })
+db.on('error', () => {
+  console.log('mongoose connection error');
+});
 
-// This is to connect the backend to an external DB hosted on https://mlab.com/
-// mongoose.connect('mongodb://qays:qays123@ds129670.mlab.com:29670/donate')
-let db = mongoose.connection // Create mongoose connection for mongodb
+db.once('open', () => {
+  console.log('mongoose connected successfully');
+});
 
-db.on('error', function () {
-  console.log('mongoose connection error')
-}) // This will apply if there is a problem on mongoose connection for mongodb
-db.once('open', function () {
-  console.log('mongoose connected successfully')
-}) // This will apply if mongoose is connected successfully for mongodb
-
-let Schema = mongoose.Schema // Create a mongoose schema
+let Schema = mongoose.Schema; // Create a mongoose schema 
 
 let equipmentSchema = new Schema({
-  name: { type: String, required: true},
-  serialNumber : {type: Number, required: true},
-  attachment : {type: String}
+  name: { type: String },
+  serialNumber: { type: Number },
+  attachment: { type: String }
 })
 
 let employeesSchema = new Schema({
-  name : {type: String},
-  nationality : {type: String},
-  jobTitle : {type: String},
-  employees: {type: String},
-  equipment: {type: String}
+  id: { type: Number },
+  name: { type: String },
+  nationality: { type: String },
+  jobTitle: { type: String }
 })
 
 let projectsSchema = new Schema({
-  name: {type: String},
-  type: {type: String},
-  fromDate: {type: Date},
-  toDate: {type: Date}
+  name: { type: String },
+  type: { type: String },
+  fromDate: { type: Date },
+  toDate: { type: Date },
+  employees: { type: String },
+  equipment: { type: String }
 })
 
 equipmentSchema = mongoose.model('equipmentSchema', equipmentSchema);
 employeesSchema = mongoose.model('employeesSchema', employeesSchema);
 projectsSchema = mongoose.model('projectsSchema', projectsSchema);
 
-module.exports.employeesSchema = employeesSchema;
 module.exports.equipmentSchema = equipmentSchema;
+module.exports.employeesSchema = employeesSchema;
 module.exports.projectsSchema = projectsSchema;
-
